e97463bf84f12e4f299f0d4d14b59cfda5135365
Nedmon12 <mentesnot.ertibu@gmail.com>
2024-01-05 14:00:39 +0300

    client query() helper

--- client.cpp
+++ client.cpp
@@ -15,6 +15,50 @@ static void die(const char *msg) {
   abort();
 }
 
+static int32_t query(int fd, const char *text) {
+  uint32_t len = (uint32_t)strlen(text);
+  if (len > k_max_msg) {
+    return -1;
+  }
+
+  char wbuf[4 + k_max_msg];
+  memcpy(wbuf, &len, 4);
+  memcpy(&wbuf[4], text, len);
+  //TODO helper function write_all() not implemented yet
+  if (int32_t err = write_all(fd, wbuf, 4 + len)) {
+    return err;
+  }
+
+  char rbuf[4 + k_max_msg + 1];
+  errno = 0;
+  //TODO helper function read_all()
+  int32_t err = read_full(fd, rbuf, 4);
+  if (err) {
+    if (errno == 0) {
+      msg("EOF");
+    } else {
+      msg("read () error");
+    }
+    return err;
+  }
+
+  memcpy(&len, rbuf, 4); //little endian
+  if (len > k_max_msg) {
+    msg("too long");
+    return -1;
+  }
+
+  err = read_full(fd, &rbuf[4], len);
+  if (err) {
+    msg("read() error");
+    return err;
+  }
+
+  rbuf[4+len] = '\0';
+  printf("server says: %s\n", &rbuf[4]);
+  return 0;
+}
+
 
 
 int main() {
